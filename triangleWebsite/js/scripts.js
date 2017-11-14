c553f8f5f1afff9c9e94d9e6915a8ac2e0c55f2c
Anna Marie Cooper and Lieghti Sharp <anna@>
2017-11-13 20:00:18 -0800

    got triangle math to work.

--- triangleWebsite/js/scripts.js
+++ triangleWebsite/js/scripts.js
@@ -1,18 +1,19 @@
-var side1 = parseInt($("#input1").val());
-var side2 = parseInt($("#input2").val());
-var side3 = parseInt($("#input3").val());
 
 $(document).ready(function() {
   $("#triangle").submit(function (event) {
     event.preventDefault();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    console.log(side1, side2, side3);
     if ((side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2 ) {
     alert ("That's no triangle!");
     } else if (side1 != side2 && side2 != side3 && side1 != side3){
     alert ("You have an Scalene triangle!");
-    } else if (side1 === side2 || side2 === side3 || side1 === side3){
-      alert ("You have an Isosceles triangle!");
     } else if (side1 === side2 && side2 === side3 && side1 === side3){
     alert ("You have an Equilateral triangle!");
+    } else if (side1 === side2 || side2 === side3 || side1 === side3){
+    alert ("You have an Isosceles triangle!");
     }
   });
 });
